b4671328488e6580ffd448e3c7c2a201e6153c0a
Byekibe <kibetpeter95@gmail.com>
2023-11-30 12:24:49 +0300

    Add id to the inline script tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
               async
               src="https://www.googletagmanager.com/gtag/js?id=G-8XTLLSMSJ7"
             />
-            <Script>
+            <Script id="google-analytics">
               window.dataLayer = window.dataLayer || []; function gtag()
               {dataLayer.push(arguments)}
               gtag('js', new Date()); gtag('config', 'G-8XTLLSMSJ7');
